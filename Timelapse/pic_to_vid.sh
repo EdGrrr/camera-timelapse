74b0ce5f159adb464d1bcf8ae170cf5993c8114f
Edward Gryspeerdt <e.gryspeerdt@imperial.ac.uk>
2023-04-20 06:46:56 +0100

    Turn off ffmpeg for low power camera

--- Timelapse/pic_to_vid.sh
+++ Timelapse/pic_to_vid.sh
@@ -14,14 +14,14 @@ mkdir -p ${MAIN_FOLDER}/videos/${DAY}
 
 
 cd ${FOLDER}
-for LABEL in A;
-do
-    if compgen -G "*${LABEL}.jpg" > /dev/null; then
-        ffmpeg -hide_banner -loglevel error -framerate 25 -pattern_type glob -i '*'"${LABEL}"'.jpg' -c:v libx264 -crf 18 -preset slow "${MAIN_FOLDER}"/videos/"${DAY}"/"${PREFIX}"_"${DAY}"_"${HOUR}${LABEL}".mp4;
-    fi
-done
+# for LABEL in A;
+# do
+#     if compgen -G "*${LABEL}.jpg" > /dev/null; then
+#         ffmpeg -hide_banner -loglevel error -framerate 25 -pattern_type glob -i '*'"${LABEL}"'.jpg' -c:v libx264 -crf 18 -preset slow "${MAIN_FOLDER}"/videos/"${DAY}"/"${PREFIX}"_"${DAY}"_"${HOUR}${LABEL}".mp4;
+#     fi
+# done
 mv *_CAL*.jpg ${MAIN_FOLDER}/cal/${DAY} 2> /dev/null
 
 cd ..
-#tar -zcvf ${HOUR}.tar.gz ${HOUR} 
+tar -zcvf ${HOUR}.tar.gz ${HOUR} 
 rm -rf ${HOUR}
