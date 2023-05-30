16c22cb26d82aac04863643282f8d1a7d6f3e552
Edward Gryspeerdt <e.gryspeerdt@imperial.ac.uk>
2023-05-30 22:47:30 +0100

    Typos

--- power/pijuice_shutdown_to_period.py
+++ power/pijuice_shutdown_to_period.py
@@ -28,11 +28,12 @@ pj.rtcAlarm.ClearAlarmFlag()
 pj.rtcAlarm.SetAlarm({'minute': (t.minute+interval)%60})
 pj.rtcAlarm.SetWakeupEnabled(True)
 
+print(f"Wakeup at {pj.rtcAlarm.GetAlarm()['data']}")
 # Remove power to PiJuice MCU IO pins
-pijuice.power.SetSystemPowerSwitch(0)
+pj.power.SetSystemPowerSwitch(0)
 
 # Remove 5V power to RPi after 20 seconds
-pijuice.power.SetPowerOff(20)
+pj.power.SetPowerOff(20)
 
 # Shut down the RPi
 os.system("sudo halt")
